29a008c4d05eb823e0fe24111442edefd8cebd95
Richard Lobb <richard.lobb@canterbury.ac.nz>
2014-06-12 20:06:54 +1200

    Add missing tests/config.php

---
+++ coderunner/tests/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Set-up config variables for running tests.
+ *
+ * @package    qtype
+ * @subpackage coderunner
+ * @copyright  2013 Richard Lobb, University of Canterbury
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+set_config('runguardsandbox_enabled', 0, 'qtype_coderunner');
+set_config('liusandbox_enabled', 0, 'qtype_coderunner');
+set_config('ideonesandbox_enabled', 0, 'qtype_coderunner');
+set_config('jobesandbox_enabled', 1, 'qtype_coderunner');
+set_config('jobe_host', 'localhost', 'qtype_coderunner');
+set_config('ideone_user', 'coderunner', 'qtype_coderunner');
+set_config('ideone_password', 'moodlequizzes', 'qtype_coderunner');
+set_config('ideone_password', 'moodlequizzes', 'qtype_coderunner');
