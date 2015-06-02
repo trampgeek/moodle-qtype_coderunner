e1db2e764bb84cf2ea566420a516e15454ab9446
Richard Lobb <richard.lobb@canterbury.ac.nz>
2015-06-02 19:01:36 +1200

    Turn off ideone sandbox testing (defunct).

--- type/coderunner/tests/config.php
+++ type/coderunner/tests/config.php
@@ -13,10 +13,10 @@ defined('MOODLE_INTERNAL') || die();
 
 set_config('runguardsandbox_enabled', 1, 'qtype_coderunner');
 set_config('liusandbox_enabled', 1, 'qtype_coderunner');
-set_config('ideonesandbox_enabled', 1, 'qtype_coderunner');
+set_config('ideonesandbox_enabled', 0, 'qtype_coderunner');
 set_config('jobesandbox_enabled', 1, 'qtype_coderunner');
 set_config('jobe_host', 'localhost', 'qtype_coderunner');
 set_config('jobe_apikey', 'test-api-key', 'qtype_coderunner');
 set_config('ideone_user', 'coderunner', 'qtype_coderunner');
 set_config('ideone_password', 'moodlequizzes', 'qtype_coderunner');
-set_config('ideone_password', 'moodlequizzes', 'qtype_coderunner');
+
