42c71ed07d9589a8f86897de73f62b255466d3a4
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-01-13 15:55:05 +1300

    Change version number to 3.1.1

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.0';
+$plugin->release = '3.1.1';
 
 $plugin->dependencies = array(
     'qbehaviour_adaptive_adapted_for_coderunner' => 2016122000
