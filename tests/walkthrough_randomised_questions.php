f0decac3ca53f83564fac55928b3ff25ca514b46
Richard Lobb <richard.lobb@canterbury.ac.nz>
2018-04-28 14:12:13 +1200

    Test addition of set_random_seed function to Twig.

--- tests/walkthrough_randomised_questions.php
+++ tests/walkthrough_randomised_questions.php
@@ -60,8 +60,59 @@ class qtype_coderunner_walkthrough_randomisation_test extends qbehaviour_walkthr
     }
 
 
-    private function add_fields($q) {
-        $q->templateparams = '{"func": "{{ random(["sqr", "mysqr"]) }}", "n": {{ 111 + random(1) }} }';
+    // A variant of the above that sets the random number seed at the start.
+    // We check that iterating over a sequence of seeds eventually results in
+    // each possible answer and that continuing with a fixed seed results in
+    // no further randomisation.
+    public function test_randomised_sqr_with_seed() {
+
+        $iters = 0;
+        $tests = array(
+            array('searchfor' => 'print(mysqr(111))', 'answer' => "def mysqr(n): return n * n"),
+            array('searchfor' => 'print(mysqr(112))', 'answer' => "def mysqr(n): return n * n"),
+            array('searchfor' => 'print(sqr(111))', 'answer' => "def sqr(n): return n * n"),
+            array('searchfor' => 'print(sqr(112))', 'answer' => "def sqr(n): return n * n"),
+        );
+
+        foreach ($tests as $test) {
+            // First, iterate the seed until the desired situation occurs
+            $found = false;
+            $seed = 1000;
+            while ($seed < 1100 && !$found) {
+                $seed++;
+                $q = test_question_maker::make_question('coderunner', 'sqr');
+                $this->add_fields($q, $seed);
+                $this->start_attempt_at_question($q, 'adaptive', 1, 1);
+                $this->render();
+                if (strpos($this->currentoutput, $test['searchfor']) !== false) {
+                    $found = true;
+                }
+            }
+
+            $this->assertTrue($found);
+
+            // Without changing seed, check that the same results occur every time
+            for ($i = 0; $i < 20; $i++) {
+                $q = test_question_maker::make_question('coderunner', 'sqr');
+                $this->add_fields($q, $seed);
+                $this->start_attempt_at_question($q, 'adaptive', 1, 1);
+                $this->render();
+                $this->assertTrue (strpos($this->currentoutput, $test['searchfor']) !== false);
+            }
+        }
+    }
+
+
+    private function add_fields($q, $seed=false) {
+        if ($seed !== false) {
+            $seeding = "{{- set_random_seed($seed) -}}\n";
+        } else {
+            $seeding = '';
+        }
+
+        $templateparams = '{"func": "{{ random(["sqr", "mysqr"]) }}", "n": {{ 111 + random(1) }} }';
+
+        $q->templateparams = $seeding . $templateparams;
         $q->hoisttemplateparams = 1;
         $q->twigall = 1;
         $q->questiontext = 'Write a function {{ func }}';
