37faaa550d1ec0e2d73d06837b9e29823dbfa238
Richard Lobb <richard.lobb@canterbury.ac.nz>
2018-04-28 14:38:37 +1200

    Add documentation of Mac control-delete key to graph HELP menu.

--- lang/en/qtype_coderunner.php
+++ lang/en/qtype_coderunner.php
@@ -163,7 +163,7 @@ $string['graphhelp'] = '- Double click at a blank space to create a new node/sta
 - Click on a link/node to edit its text.
 - Typing _ followed by a digit makes that digit a subscript.
 - Typing \\epsilon creates an epsilon character (and similarly for \\alpha, \\beta etc).
-- Click on a link/node then press the Delete key to remove it.';
+- Click on a link/node then press the Delete key to remove it (or function-delete on a Mac).';
 
 $string['goodemptyprecheck'] = 'Passed';
 $string['gotcolhdr'] = 'Got';
