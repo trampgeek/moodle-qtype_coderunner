7bc3b147cf3522b789e4ddb9c7054ad326382b77
Richard Lobb <trampgeek@gmail.com>
2018-10-16 21:32:35 +1300

    Set version number back to 3.5.2.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5.2+';
+$plugin->release = '3.5.2';
 
 $plugin->dependencies = array(
     'qbehaviour_adaptive_adapted_for_coderunner' => 2018042800
