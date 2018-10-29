d56e9e83bd3035de8bd38a9ca515f9bca96ef290
Richard Lobb <trampgeek@gmail.com>
2018-10-29 17:16:37 +1300

    Refactor new install/upgrade code.

--- db/upgradelib.php
+++ db/upgradelib.php
@@ -37,17 +37,23 @@ require_once($CFG->dirroot . '/lib/questionlib.php');
  * @return bool true if successful
  */
 function update_question_types() {
-    global $DB;
-
-    $success = true;
     $systemcontext = context_system::instance();
     $systemcontextid = $systemcontext->id;
     if (!$systemcontextid) {
         $systemcontextid = 1; // HACK ALERT: occurs when phpunit initialising itself.
     }
 
-    // Delete all existing prototypes, namely any questions in the system
-    // context with names containing the string PROTOTYPE_
+    delete_existing_prototypes($systemcontextid);
+    $topid = find_or_make_top($systemcontextid);
+    $prototypes_category = find_or_make_prototype_category($systemcontextid, $topid);
+    load_new_prototypes($systemcontextid, $prototypes_category);
+    return true;
+}
+
+
+// Delete all existing prototypes in the given (system) context.
+function delete_existing_prototypes($systemcontextid) {
+    global $DB;
     $query = "SELECT q.id
               FROM {question} q JOIN {question_categories} cats
               ON q.category = cats.id
@@ -58,59 +64,52 @@ function update_question_types() {
         $DB->delete_records('question_coderunner_options', array('questionid' => $question->id));
         $DB->delete_records('question', array('id' => $question->id));
     }
+}
 
-    // Find or create the top category for the system context
+
+// Find or create the top category for the system context
+// This may involve renaming an existing CR_PROTOTYPES category to 'top'
+// if there's one already there but no actual top.
+// Return the topid.
+function find_or_make_top($systemcontextid) {
+    global $DB;
     $topid = -1;
+    $prototypecategoryid = -1;
     $tops = $DB->get_records('question_categories',
             array('contextid' => $systemcontextid, 'parent' => 0));
 
-    if (count($tops) == 0) {
-        // If there are no top records, make one now
-        $category = new stdClass();
-        $category->name = 'top'; // A non-real name for the top category. It will be localised at the display time.
-        $category->info = '';
-        $category->contextid = $systemcontextid;
-        $category->parent = 0;
-        $category->sortorder = 0;
-        $category->stamp = make_unique_id_code();
-        $topid = $DB->insert_record('question_categories', $category);
-    } else if (count($tops) == 1) {
-        // If there's a single top record, check its name. It could be
-        // CR_PROTOTYPES category resulting from installing an older
-        // CodeRunner on top of a fresh Moodle 3.5. If so, rename it to 'top'
-        // and set various other fields to their standard top values.
-        $cats = array_values($tops);
-        $category = $cats[0];
-        $topid = $category->id;
-        if ($category->name == 'CR_PROTOTYPES') {
-            $category->name = 'top'; // A non-real name for the top category. It will be localised at the display time.
-            $category->info = '';
-            $category->contextid = $systemcontextid;
-            $category->parent = 0;
-            $category->sortorder = 999;
-            $category->stamp = make_unique_id_code();
-            $DB->update_record('question_categories', $category);
-        }
-    } else {
-        // There is more than one top record. This might be a Moodle version
-        // prior to 3.5 or it might be an error situation from installing an
-        // earlier version of CodeRunner on Moodle 3.5, which allows only
-        // one top record in a context.
-        // Try to identify one of them as a 'real' top category.
-        foreach ($tops as $id => $category) {
-            if (strtolower($category->name) === 'top') {
-                $topid = $category->id;
-                break;
-            }
+    foreach ($tops as $id => $category) {
+        if (strtolower($category->name) === 'top') {
+            $topid = $category->id;
+            break;
+        } else if ($category->name == 'CR_PROTOTYPES') {
+            $prototypecategoryid = $category->id;
+            $prototypecat = $category;
         }
     }
+    if ($topid === -1 && $prototypecategoryid !== -1) {
+        // No top found, but we do have an existing CR_PROTOTYPES category
+        // so use that. This is to handle the situation where an older
+        // version of CodeRunner was installed on Moodle 3.5, and the
+        // CR_PROTOTYPES category was then used as the 'top' category. Other
+        // content may then have been added and we don't want to lose it.
+        $topid = $prototypecategoryid;
+        make_existing_category_top($prototypecat, $systemcontextid);
+    }
 
+    // If we couldn't find a top category or make one from an existing
+    // CR_PROTOTYPES category, create one now.
     if ($topid === -1) {
-        throw new coding_exception("CodeRunner install/upgrade failed: couldn't find top category for system context");
+        $topid = make_new_top($systemcontextid);
     }
+    return $topid;
+}
 
-    // Now create CR_PROTOTYPES category if it doesn't exist or, if it already
-    // exists, ensure that it is a child of the top category
+
+// Create CR_PROTOTYPES category if it doesn't exist or, if it already
+// exists, ensure that it is a child of the top category
+function find_or_make_prototype_category($systemcontextid, $topid) {
+    global $DB;
     $category = $DB->get_record('question_categories',
                 array('contextid' => $systemcontextid, 'name' => 'CR_PROTOTYPES'));
     if ($category) {
@@ -131,17 +130,48 @@ function update_question_types() {
         }
         $category = $DB->get_record('question_categories', array('id' => $prototypecategoryid));
     }
+    return $category;
+}
 
-    // Lastly, load any files in the db directory ending with _PROTOTYPES.xml.
+
+// Create a new top category and return its id
+function make_new_top($systemcontextid) {
+    global $DB;
+    $category = new stdClass();
+    $category->name = 'top'; // A non-real name for the top category. It will be localised at the display time.
+    $category->info = '';
+    $category->contextid = $systemcontextid;
+    $category->parent = 0;
+    $category->sortorder = 0;
+    $category->stamp = make_unique_id_code();
+    $topid = $DB->insert_record('question_categories', $category);
+    return $topid;
+}
+
+// Given a category (which should always be CR_PROTOTYPES), rename it as
+// 'top' and rewrite it to the database with various other attributes
+// set appropriately.
+function make_existing_category_top($category, $systemcontextid) {
+    global $DB;
+    $category->name = 'top'; // A non-real name for the top category. It will be localised at the display time.
+    $category->info = '';
+    $category->contextid = $systemcontextid;
+    $category->parent = 0;
+    $category->sortorder = 999;
+    $category->stamp = make_unique_id_code();
+    $DB->update_record('question_categories', $category);
+}
+
+
+// Load any files in the db directory ending with _PROTOTYPES.xml.
+function load_new_prototypes($systemcontextid, $prototype_category) {
     $dbfiles = scandir(__DIR__);
     foreach ($dbfiles as $file) {
         if (strpos(strrev($file), strrev('_PROTOTYPES.xml')) === 0) {
             $filename = __DIR__ . '/' . $file;
-            load_questions($category, $filename, $systemcontextid);
+            load_questions($prototype_category, $filename, $systemcontextid);
         }
     }
-
-    return $success;
 }
 
 /**
--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018102901;
+$plugin->version  = 2018102902;
 $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
