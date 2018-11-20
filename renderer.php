e2a5bee85d9a48318133b69ce9af6c0515f10aec
Richard Lobb <trampgeek@gmail.com>
2018-11-20 13:20:39 +1300

    On-going edits for file uploading.

--- renderer.php
+++ renderer.php
@@ -158,6 +158,18 @@ class qtype_coderunner_renderer extends qtype_renderer {
                     array('class' => 'validationerror'));
         }
 
+        // Add file upload controls if attachments are allowed
+        $files = '';
+        if ($question->attachments) {
+            if (empty($options->readonly)) {
+                $files = $this->files_input($qa, $question->attachments, $options);
+
+            } else {
+                $files = $this->files_read_only($qa, $options);
+            }
+            $qtext .= html_writer::tag('div', $files, array('class' => 'attachments'));
+        }
+
         // Initialise any JavaScript UI. Default is Ace unless uiplugin is explicitly
         // set and is neither the empty string nor the value 'none'.
         // Thanks to Ulrich Dangel for the original implementation of the Ace code editor.
@@ -434,6 +446,74 @@ class qtype_coderunner_renderer extends qtype_renderer {
         return $html;
     }
 
+
+    /**
+     * Displays any attached files when the question is in read-only mode.
+     * @param question_attempt $qa the question attempt to display.
+     * @param question_display_options $options controls what should and should
+     *      not be displayed. Used to get the context.
+     */
+    public function files_read_only(question_attempt $qa, question_display_options $options) {
+        $files = $qa->get_last_qt_files('attachments', $options->context->id);
+        $output = array();
+
+        foreach ($files as $file) {
+            $output[] = html_writer::tag('p', html_writer::link($qa->get_response_file_url($file),
+                    $this->output->pix_icon(file_file_icon($file), get_mimetype_description($file),
+                    'moodle', array('class' => 'icon')) . ' ' . s($file->get_filename())));
+        }
+        return implode($output);
+    }
+
+    /**
+     * Displays the input control for when the student is allowed to upload files.
+     * @param question_attempt $qa the question attempt to display.
+     * @param int $numallowed the maximum number of attachments allowed. -1 = unlimited.
+     * @param question_display_options $options controls what should and should
+     *      not be displayed. Used to get the context.
+     */
+    public function files_input(question_attempt $qa, $numallowed,
+            question_display_options $options) {
+        global $CFG;
+        require_once($CFG->dirroot . '/lib/form/filemanager.php');
+
+        $question = $qa->get_question();
+        $pickeroptions = new stdClass();
+        $pickeroptions->mainfile = null;
+        $pickeroptions->maxfiles = $numallowed;
+        $pickeroptions->maxbytes = $this->compute_max_bytes($question->maxfilesize);
+        $pickeroptions->itemid = $qa->prepare_response_files_draft_itemid(
+                'attachments', $options->context->id);
+        $pickeroptions->context = $options->context;
+        $pickeroptions->return_types = FILE_INTERNAL | FILE_CONTROLLED_LINK;
+
+        $pickeroptions->itemid = $qa->prepare_response_files_draft_itemid(
+                'attachments', $options->context->id);
+        $pickeroptions->accepted_types = $qa->get_question()->filetypeslist;
+
+        $fm = new form_filemanager($pickeroptions);
+        $filesrenderer = $this->page->get_renderer('core', 'files');
+
+        $text = '';
+        if (!empty($qa->get_question()->filetypeslist)) {
+            $text = html_writer::tag('p', get_string('acceptedfiletypes', 'qtype_coderunner'));
+            $filetypesutil = new \core_form\filetypes_util();
+            $filetypes = $qa->get_question()->filetypeslist;
+        }
+        return $filesrenderer->render($fm). html_writer::empty_tag(
+                'input', array('type' => 'hidden', 'name' => $qa->get_qt_field_name('attachments'),
+                'value' => $pickeroptions->itemid)) . $text;
+    }
+
+
+    private function compute_max_bytes($sizespecifier)
+    {
+        debugging("Maxsize: " . $sizespecifier);
+        $bits = explode(' ', $sizespecifier);
+        $size = intval($bits[0]);
+        $multiplier = $bits[1] === 'kB' ? 1000 : ($bits[1] === 'MB' ? 1000000 : debugging("Unknown filemaxsize unit"));
+        return $size * $multiplier;
+    }
 
     /**
      *
