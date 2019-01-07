d6f3f65f6641a20324a2a3f08e0be993318160a2
Richard Lobb <trampgeek@gmail.com>
2019-01-07 20:25:26 +1300

    Update version date.

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018121002;
+$plugin->version  = 2019010702;
 $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
