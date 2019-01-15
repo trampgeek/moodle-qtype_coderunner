4258eaf5c6987599f1c41285c52e51b5b12560e0
Richard Lobb <trampgeek@gmail.com>
2019-01-15 20:27:56 +1300

    Change version number and update change history for version 3.6.0.

--- version.php
+++ version.php
@@ -22,12 +22,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019011103;
+$plugin->version  = 2019011500;
 $plugin->requires = 2015051100;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5.4';
+$plugin->release = '3.6.0';
 
 $plugin->dependencies = array(
     'qbehaviour_adaptive_adapted_for_coderunner' => 2018042800
