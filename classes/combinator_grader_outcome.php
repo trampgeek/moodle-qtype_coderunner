3f9b0172fd8968d1001e46c1826de124dba7393f
Richard Lobb <trampgeek@gmail.com>
2019-10-03 18:23:35 +1300

    Bug fix: combinator grader was issuing spurious error messages if no result table was present.

--- classes/combinator_grader_outcome.php
+++ classes/combinator_grader_outcome.php
@@ -133,6 +133,9 @@ class qtype_coderunner_combinator_grader_outcome extends qtype_coderunner_testin
     // specifier in html_wrapper objects leaving other cells unchanged.
     // ishidden and iscorrect columns are copied across unchanged.
     private function format_table($table) {
+        if (empty($table)) {
+            return $table;
+        }
         if (!$this->columnformats) {
             $newtable = $table;
         } else {
