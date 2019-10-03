4b44e98b40f63b02971d4969ccff16880481d36c
Richard Lobb <trampgeek@gmail.com>
2019-10-03 19:32:29 +1300

    Replace pdf-rendering question loader with one that provides a clickable link to the pdf, using a data-uri.

--- amd/src/ajaxquestionloader.js
+++ amd/src/ajaxquestionloader.js
@@ -27,123 +27,13 @@
 
 
 define(['jquery'], function ($) {
-    var pdfjsLib = window['pdfjs-dist/build/pdf'];
-    var pdfDoc = null,
-            pageNumSpan,
-            pageRendering = false,
-            pageNumPending = null,
-            scale = 1.2,
-            canvas,
-            next,
-            previous,
-            pageNum = 1,
-            numPagesSpan,
-            ctx,
-            pageRendering;
-
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-
-    /**
-     * Get page info from document, resize canvas accordingly, and render page.
-     * @param num Page number.
-     */
-    function renderPage(num) {
-        pageRendering = true;
-        // Using promise to fetch the page
-        pdfDoc.getPage(num).then(function (page) {
-            var viewport = page.getViewport({scale: scale});
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: ctx,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-
-            // Wait for rendering to finish
-            renderTask.promise.then(function () {
-                pageRendering = false;
-                pageNumSpan.html('' + num);  // Update page counter
-                if (num == 1) {
-                    previous.attr('disabled', true);
-                } else {
-                    previous.removeAttr('disabled');
-                }
-                if (num == pdfDoc.numPages) {
-                    next.attr('disabled', true);
-                } else {
-                    next.removeAttr('disabled');
-                }
-                if (pageNumPending !== null) {
-                    // New page rendering is pending
-                    renderPage(pageNumPending);
-                    pageNumPending = null;
-                }
-            });
-        });
-    }
-
-    /**
-     * If another page rendering in progress, waits until the rendering is
-     * finised. Otherwise, executes rendering immediately.
-     */
-    function queueRenderPage(num) {
-        if (pageRendering) {
-            pageNumPending = num;
-        } else {
-            renderPage(num);
-        }
-    }
-
-    /**
-     * Displays previous page.
-     */
-    function onPrevPage() {
-        if (pageNum <= 1) {
-            return;
-        }
-        pageNum--;
-        queueRenderPage(pageNum);
-    }
-
-    /**
-     * Displays next page.
-     */
-    function onNextPage() {
-        if (pageNum >= pdfDoc.numPages) {
-            return;
-        }
-        pageNum++;
-        queueRenderPage(pageNum);
-    }
-
     function loadQuestionText(qid, divId, questionFilename) {
         var questionTextDiv = $('#' + divId),
-            qDiv;
+            errorDiv = '<div style="colour:red">Failed to load problem spec<div>';
         if (questionTextDiv.length != 1) {
-            window.alert("Can't load question text. Expected div not found");
+            questionTextDiv.append(errorDiv);
             return;
         }
-        qDiv = '<div>\n' +
-            '<button type="button" class="qtype_coderunner_previous">Previous</button>\n' +
-            '<button type="button" class="qtype_coderunner_next">Next</button>' +
-            '&nbsp; &nbsp;' +
-            '<span>Page: <span class="qtype_coderunner_pagenum"></span> / <span ' +
-            'class="qtype_coderunner_numpages"></span></span>\n' +
-            '</div>\n' +
-            '<canvas id="qtype_coderunner_problemspec_' + qid + '"></canvas>\n' +
-            '</div>\n';
-        questionTextDiv.append(qDiv);
-        canvas = document.getElementById('qtype_coderunner_problemspec_' + qid);
-        next = $('#' + divId + ' button.qtype_coderunner_next');
-        previous = $('#' + divId + ' button.qtype_coderunner_previous');
-        pageNumSpan = $('#' + divId + ' span.qtype_coderunner_pagenum');
-        numPagesSpan = $('#' + divId + ' span.qtype_coderunner_numpages');
-        ctx = canvas.getContext('2d');
-        next.click(onNextPage);
-        previous.click(onPrevPage);
         $.getJSON(M.cfg.wwwroot + '/question/type/coderunner/problemspec.php',
                 {
                     questionid: qid,
@@ -151,27 +41,18 @@ define(['jquery'], function ($) {
                     filename: questionFilename
                 },
                 function (response) {
-                    var pdfcontents;
-                    var loadingTask;
-                    if (response == "FILE NOT FOUND") {
-                        window.alert("Problem spec file not found");
-                    } else if (response.filecontentsb64) {
-                        pdfcontents = atob(response.filecontentsb64);
-                        loadingTask = pdfjsLib.getDocument({data: pdfcontents});
-                        loadingTask.promise.then(function (pdf) {
-                            pdfDoc = pdf;
-                            numPagesSpan.html('' + pdf.numPages);
-                            renderPage(1);
-                        });
-
+                    if (response.filecontentsb64) {
+                        questionTextDiv.append(
+                          '<div><a href="data:application/pdf;base64,' +
+                          response.filecontentsb64 + '">View problem spec</a></div>');
                     } else {
-                        window.alert("Bad response object");
+                        questionTextDiv.append(errorDiv);
                     }
 
                 }
         ).fail(function () {
             // AJAX failed. We're dead, Fred.
-            window.alert('Failed to load problem spec');
+            questionTextDiv.append(errorDiv);
         });
     }
 
