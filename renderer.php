e8d6d9000a926deda3d0fb4f577e9547a5d4e635
Luca Bösch <luca.boesch@bfh.ch>
2020-01-21 07:34:25 +0100

    Style the answer reset button properly.

--- renderer.php
+++ renderer.php
@@ -724,7 +724,7 @@ class qtype_coderunner_renderer extends qtype_renderer {
             'id' => $buttonid,
             'name' => $buttonid,
             'value' => get_string('reset', 'qtype_coderunner'),
-            'class' => 'answer_reset_btn',
+            'class' => 'answer_reset_btn btn btn-secondary',
             'data-reload-text' => $preload);
         $html = html_writer::empty_tag('input', $attributes);
 
