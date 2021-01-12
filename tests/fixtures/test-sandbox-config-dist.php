0cbb8bbabe17bf02bc489c5897a9a145e1acf30c
Richard Lobb <trampgeek@gmail.com>
2021-01-12 14:55:25 +1300

    Turn off jobe sandbox in the default config dist (requested by a user running continuous integration but without their own jobe server). This means it must be enabled in the local clone of the file.

--- tests/fixtures/test-sandbox-config-dist.php
+++ tests/fixtures/test-sandbox-config-dist.php
@@ -36,7 +36,7 @@ defined('MOODLE_INTERNAL') || die();
 
 set_config('default_penalty_regime', '10, 20, ...', 'qtype_coderunner');
 set_config('ideonesandbox_enabled', 0, 'qtype_coderunner');
-set_config('jobesandbox_enabled', 1, 'qtype_coderunner');
+set_config('jobesandbox_enabled', 0, 'qtype_coderunner');
 set_config('jobe_host', 'localhost', 'qtype_coderunner');
 set_config('jobe_apikey', '2AAA7A5415B4A9B394B54BF1D2E9D', 'qtype_coderunner');
 set_config('ideone_user', 'coderunner', 'qtype_coderunner');
