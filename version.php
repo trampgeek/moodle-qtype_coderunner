834800405f526ea6de35d239689f1888a75d386b
Richard Lobb <trampgeek@gmail.com>
2021-08-29 16:29:25 +1200

    Update date of new version.

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021050800;
+$plugin->version  = 2021082900;
 $plugin->requires = 2019111800;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
