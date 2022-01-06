f54d2f67860fe028b748c7286573c38b570a934e
Richard Lobb <trampgeek@gmail.com>
2022-01-06 15:12:57 +1300

    Define an event for a web-service sandbox run.

---
+++ classes/event/sandbox_webservice_exec.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A sandbox service task execution event.
+ *
+ * @package    qtype_coderunner
+ * @copyright  2022 Richard Lobb, University of Canterbury
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace qtype_coderunner\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+class sandbox_webservice_exec extends \core\event\base {
+    /**
+     * Init method.
+     */
+    protected function init() {
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+        $this->data['crud'] = 'c';
+    }
+
+    /**
+     * Returns localised event name.
+     *
+     * @return string
+     */
+    public static function get_name() {
+        return get_string('event_sandboxwebserviceexec', 'qtype_coderunner');
+    }
+
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
+    public function get_description() {
+        return "The user with id '$this->userid' executed a job via the CodeRunner sandbox web service.";
+    }
+}
