2c3b1b2709701185d8a2844afa6ae72168484be4
Tim Hunt <T.J.Hunt@open.ac.uk>
2022-01-14 13:48:54 +0000

    Allow long-running scripts to do progressive output
    Depending on how your servers are set up, this change may not be needed,
    but in general any Moodle script that might run for a long time while
    doing progressive output needs this.

--- downloadquizattempts.php
+++ downloadquizattempts.php
@@ -28,9 +28,10 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+define('NO_OUTPUT_BUFFERING', true);
+
 require_once(__DIR__.'/../../../config.php');
 require_once($CFG->libdir . '/questionlib.php');
-
 
 // Login and check permissions.
 $context = context_system::instance();
