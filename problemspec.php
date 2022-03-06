ec669cc4e322de5fe830dbe07d8f967bbef7e318
Richard Lobb <trampgeek@gmail.com>
2022-03-06 15:16:13 +1300

    Fix regression: AJAX access to problem spec in programming contest problems was broken by addition of the Moodle Internal check.

--- problemspec.php
+++ problemspec.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with CodeRunner.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Script to return the spec for the currently-rendered coderunner question,
  * assumed to be present in a zip support file. Since pdfs are potentially
