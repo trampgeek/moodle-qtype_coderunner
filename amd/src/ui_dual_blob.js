f07d0f5e703731bc5dc25c57f1ccefc97444c9c5
jhn27 <jamesnapier64@gmail.com>
2022-12-01 12:22:18 +1300

    Ace added using two UI wrappers, each containing an ACE UI

--- amd/src/ui_dual_blob.js
+++ amd/src/ui_dual_blob.js
@@ -274,8 +274,7 @@ define(['jquery'], function ($) {
     DualBlobUi.prototype.sync = function () {
         const t = this;
         let serialisation = {};
-        serialisation['answer_code'] = [this.answerEditor.sync()];
-        serialisation['test_code'] = [this.scratchpadEditor.sync()];
+
         this.getFields().each(function () {
             const name = $(this).attr('name');
             const type = $(this).attr('type');
@@ -395,31 +394,28 @@ define(['jquery'], function ($) {
 
         // Main ide.
         const divHtml = "<div></div>";
-        //const answerTextHtml = htmlTextArea('answer_code', preload['answer_code']);
+        const answerTextAreaHtml = htmlTextArea('answer_code', preload['answer_code']);
         const showButtonHtml = "<a class='coderunner-ui-element' " +
                 `name='show_hide'>▼${this.spName}</a>`;
+        const answerTextArea = $(answerTextAreaHtml);
         const showButton = $(showButtonHtml);
         const t = this;
+        
         showButton.click(function () {
             const arrow = $(t.scratchpadDiv).is(':visible') ? '▶' : '▼';
             t.scratchpadDiv.toggle();
             showButton.html(arrow + t.spName);
         });
-        const answerEditorNode = $(divHtml);
-        this.answerEditor = new AceWrapper(answerEditorNode, 
-                                            this.textareaId, 
-                                            preload['answer_code'][0], 
-                                            500, 
-                                            500,
-                                            {lang: 'python3'});
+
         this.blobDiv = $(divHtml);
         this.blobDiv.css({
             resize: 'none',
             height: this.height,
             width: "100%"
         });
-        //this.blobDiv.append([$(answerTextHtml), showButton]);
-        this.blobDiv.append([answerEditorNode, showButton]);
+        this.answerTextArea = answerTextArea;
+        this.blobDiv.append([answerTextArea, showButton]);
+
 
         // Scratchpad.
         this.scratchpadDiv = $(divHtml);
@@ -427,14 +423,7 @@ define(['jquery'], function ($) {
             this.scratchpadDiv.hide();
             showButton.html(`▶${this.spName}`);
         }
-        //const testCodeHtml = htmlTextArea('test_code', preload['test_code']);
-        const scratchpadEditorNode = $(divHtml);
-        this.scratchpadEditor = new AceWrapper(scratchpadEditorNode, 
-                                            this.textareaId, 
-                                            preload['test_code'][0], 
-                                            500, 
-                                            500,
-                                            {lang: 'python3'});
+        const testCodeHtml = htmlTextArea('test_code', preload['test_code']);
         const prefixAnsHtml = htmlInput('prefix_ans', this.spPrefixName, preload['prefix_ans'], 'checkbox');
 
         const runButton = $("<button type='button' " +
@@ -449,9 +438,20 @@ define(['jquery'], function ($) {
         const outputDisplayArea = $("<pre style='width:100%;white-space:pre-wrap;background-color:#eff;" +
                 "border:1px gray;padding:5px;overflow-wrap:break-word;max-height:600px;overflow:auto;'></pre>");
         outputDisplayArea.hide();
-        this.scratchpadDiv.append([scratchpadEditorNode, runButton, $(prefixAnsHtml), outputDisplayArea]);
+        this.spCodeTextArea = $(testCodeHtml);
+        this.scratchpadDiv.append([this.spCodeTextArea, runButton, $(prefixAnsHtml), outputDisplayArea]);
 
         this.blobDiv.append(this.scratchpadDiv);
+        
+        //console.log(t.answerTextArea.attr('id'));
+        const answerTextAreaId = this.textAreaId = '-answer-code';
+        const spTextAreaId = this.textAreaId = '-sp-code';
+        this.answerTextArea.attr('id', answerTextAreaId);
+        this.spCodeTextArea.attr('id', spTextAreaId);
+        require(['qtype_coderunner/userinterfacewrapper'], function(uiWrapper) {
+            t.answerCodeUi = uiWrapper.newUiWrapper('ace', answerTextAreaId);
+            t.spCodeUi = uiWrapper.newUiWrapper('ace', spTextAreaId);
+        });
     };
 
     DualBlobUi.prototype.resize = function () {}; // Nothing to see here. Move along please.
@@ -471,259 +471,6 @@ define(['jquery'], function ($) {
         this.sync();
         $(this.blobDiv).remove();
         this.blobDiv = null;
-    };
-    
-    
-    
-     /**
-     * Constructor for the Ace wrapper object.
-     * @param {DOMObject} textareaId The ID of the HTML textarea element to be wrapped.
-     * @param {DOMObject} the div for ace to manage.
-     * @param {int} w The width in pixels of the textarea.
-     * @param {int} h The height in pixels of the textarea.
-     * @param {object} params The UI parameter object.
-     */
-    function AceWrapper(div, textareaId, value, w, h, params) {
-        const ACE_DARK_THEME = 'ace/theme/tomorrow_night';
-        const ACE_LIGHT_THEME = 'ace/theme/textmate';
-
-        var textarea = $(document.getElementById(textareaId)),
-            wrapper = $(document.getElementById(textareaId + '_wrapper')),
-            focused = textarea[0] === document.activeElement,
-            lang = params.lang,
-            session,
-            t = this;  // For embedded callbacks.
-
-        try {
-            window.ace.require("ace/ext/language_tools");
-            this.modelist = window.ace.require('ace/ext/modelist');
-
-            this.textarea = textarea;
-            this.enabled = false;
-            this.contents_changed = false;
-            this.capturingTab = false;
-            this.clickInProgress = false;
-
-            this.editNode = div; // Ace editor manages this
-            this.editNode.css({
-                resize: 'none',
-                height: h,
-                width: "100%"
-            });
-
-            this.editor = window.ace.edit(this.editNode.get(0));
-            if (textarea.prop('readonly')) {
-                this.editor.setReadOnly(true);
-            }
-
-            this.editor.setOptions({
-                enableBasicAutocompletion: true,
-                newLineMode: "unix",
-            });
-            this.editor.$blockScrolling = Infinity;
-
-            session = this.editor.getSession();
-            session.setValue(value);
-
-            // Set theme if available (not currently enabled).
-            if (params.theme) {
-                this.editor.setTheme("ace/theme/" + params.theme);
-            }
-
-            // Set theme to dark if user-prefers-color-scheme is dark,
-            // else use the uiParams theme if provided else use light.
-            if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                this.editor.setTheme(ACE_DARK_THEME);
-            } else if (params.theme) {
-                this.editor.setTheme("ace/theme/" + params.theme);
-            } else {
-                this.editor.setTheme(ACE_LIGHT_THEME);
-            }
-
-            this.setLanguage(lang);
-
-            this.setEventHandlers(textarea);
-            this.captureTab();
-
-            // Try to tell Moodle about parts of the editor with z-index.
-            // It is hard to be sure if this is complete. ACE adds all its CSS using JavaScript.
-            // Here, we just deal with things that are known to cause a problem.
-            // Can't do these operations until editor has rendered. So ...
-            this.editor.renderer.on('afterRender', function() {
-                var gutter =  wrapper.find('.ace_gutter');
-                if (gutter.hasClass('moodle-has-zindex')) {
-                    return;  // So we only do what follows once.
-                }
-                gutter.addClass('moodle-has-zindex');
-
-                if (focused) {
-                    t.editor.focus();
-                    t.editor.navigateFileEnd();
-                }
-                t.aceLabel = wrapper.find('.answerprompt');
-                t.aceLabel.attr('for', 'ace_' + textareaId);
-
-                t.aceTextarea = wrapper.find('.ace_text-input');
-                t.aceTextarea.attr('id', 'ace_' + textareaId);
-            });
-
-            this.fail = false;
-        }
-        catch(err) {
-            // Something ugly happened. Probably ace editor hasn't been loaded
-            this.fail = true;
-        }
-    }
-
-
-    AceWrapper.prototype.failed = function() {
-        return this.fail;
-    };
-
-    AceWrapper.prototype.failMessage = function() {
-        return 'ace_ui_notready';
-    };
-
-    // Sync to TextArea
-    AceWrapper.prototype.sync = function() {
-       return this.editor.getValue();
-    };
-
-    AceWrapper.prototype.setLanguage = function(language) {
-        var session = this.editor.getSession(),
-            mode = this.findMode(language);
-        if (mode) {
-            session.setMode(mode.mode);
-        }
-    };
-
-    AceWrapper.prototype.getElement = function() {
-        return this.editNode;
-    };
-
-    AceWrapper.prototype.captureTab = function () {
-        this.capturingTab = true;
-        this.editor.commands.bindKeys({'Tab': 'indent', 'Shift-Tab': 'outdent'});
-    };
-
-    AceWrapper.prototype.releaseTab = function () {
-        this.capturingTab = false;
-        this.editor.commands.bindKeys({'Tab': null, 'Shift-Tab': null});
-    };
-
-    AceWrapper.prototype.setEventHandlers = function () {
-        var TAB = 9,
-            ESC = 27,
-            KEY_M = 77,
-            t = this;
-
-        this.editor.getSession().on('change', function() {
-            t.textarea.val(t.editor.getSession().getValue());
-            t.contents_changed = true;
-        });
-
-        this.editor.on('blur', function() {
-            if (t.contents_changed) {
-                t.textarea.trigger('change');
-            }
-        });
-
-        this.editor.on('mousedown', function() {
-            // Event order seems to be (\ is where the mouse button is pressed, / released):
-            // Chrome: \ mousedown, mouseup, focusin / click.
-            // Firefox/IE: \ mousedown, focusin / mouseup, click.
-            t.clickInProgress = true;
-        });
-
-        this.editor.on('focus', function() {
-            if (t.clickInProgress) {
-                t.captureTab();
-            } else {
-                t.releaseTab();
-            }
-        });
-
-        this.editor.on('click', function() {
-            t.clickInProgress = false;
-        });
-
-        this.editor.container.addEventListener('keydown', function(e) {
-            if (e.which === undefined || e.which !== 0) { // Normal keypress?
-                if (e.keyCode === KEY_M && e.ctrlKey && !e.altKey) {
-                    if (t.capturingTab) {
-                        t.releaseTab();
-                    } else {
-                        t.captureTab();
-                    }
-                    e.preventDefault(); // Firefox uses this for mute audio in current browser tab.
-                }
-                else if (e.keyCode === ESC) {
-                    t.releaseTab();
-                }
-                else if (!(e.shiftKey || e.ctrlKey || e.altKey || e.keyCode == TAB)) {
-                    t.captureTab();
-                }
-            }
-        }, true);
-    };
-
-    AceWrapper.prototype.destroy = function () {
-        var focused;
-        if (!this.fail) {
-            // Proceed only if this wrapper was correctly constructed
-            focused = this.editor.isFocused();
-            this.textarea.val(this.editor.getSession().getValue()); // Copy data back
-            this.editor.destroy();
-            $(this.editNode).remove();
-            if (focused) {
-                this.textarea.focus();
-                this.textarea[0].selectionStart = this.textarea[0].value.length;
-            }
-        }
-    };
-
-    AceWrapper.prototype.hasFocus = function() {
-        return this.editor.isFocused();
-    };
-
-    AceWrapper.prototype.findMode = function (language) {
-        var candidate,
-            filename,
-            result,
-            candidates = [], // List of candidate modes.
-            nameMap = {
-                'octave': 'matlab',
-                'nodejs': 'javascript',
-                'c#': 'cs'
-            };
-
-        if (typeof language !== 'string') {
-            return undefined;
-        }
-        if (language.toLowerCase() in nameMap) {
-            language = nameMap[language.toLowerCase()];
-        }
-
-        candidates = [language, language.replace(/\d+$/, "")];
-        for (var i = 0; i < candidates.length; i++) {
-            candidate = candidates[i];
-            filename = "input." + candidate;
-            result = this.modelist.modesByName[candidate] ||
-                this.modelist.modesByName[candidate.toLowerCase()] ||
-                this.modelist.getModeForPath(filename) ||
-                this.modelist.getModeForPath(filename.toLowerCase());
-
-            if (result && result.name !== 'text') {
-                return result;
-            }
-        }
-        return undefined;
-    };
-
-    AceWrapper.prototype.resize = function(w, h) {
-        this.editNode.outerHeight(h);
-        this.editNode.outerWidth(w);
-        this.editor.resize();
     };
 
     return {
