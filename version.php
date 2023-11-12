5c343c265d4f03ca19dd4441fcbf12f0256fde7f
Richard Lobb <trampgeek@gmail.com>
2023-11-12 14:08:36 +1300

    Tweak version date.

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2023110900;
+$plugin->version  = 2023111200;
 $plugin->requires = 2022041900;
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
