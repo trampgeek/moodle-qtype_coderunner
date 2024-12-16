7d14a647a40c28d072f4cc21f7c61b430cfd88d7
Richard Lobb <trampgeek@gmail.com>
2024-12-17 11:38:44 +1300

    Add a couple more utility queries.

--- ucprivate/miscqueries.sql
+++ ucprivate/miscqueries.sql
@@ -32,3 +32,32 @@ WHERE q.id IN (
         WHERE crs.shortname = 'COSC131-25S1'
     ) AS subquery
 );
+
+# Update coderunner options template parameters
+UPDATE mdl_question_coderunner_options o
+SET templateparams = REPLACE(o.templateparams, '["pylint"]', '["ruff"]')
+WHERE o.questionid IN (
+    SELECT subquery.id
+    FROM (
+        SELECT q.id
+        FROM mdl_context ctx
+        JOIN mdl_course crs ON crs.id = ctx.instanceid
+        JOIN mdl_question_categories qc ON qc.contextid = ctx.id
+        JOIN mdl_question_bank_entries qbe ON qbe.questioncategoryid = qc.id
+        JOIN mdl_question_versions qv ON qv.questionbankentryid = qbe.id
+        JOIN mdl_question q ON q.id = qv.questionid
+        WHERE crs.shortname = 'COSC131-25S1'
+    ) AS subquery
+);
+
+# All questions that match a particular template parameter string
+SELECT q.id, q.name
+FROM mdl_context ctx
+JOIN mdl_course crs ON crs.id = ctx.instanceid
+JOIN mdl_question_categories qc ON qc.contextid = ctx.id
+JOIN mdl_question_bank_entries qbe ON qbe.questioncategoryid = qc.id
+JOIN mdl_question_versions qv ON qv.questionbankentryid = qbe.id
+JOIN mdl_question q ON q.id = qv.questionid
+JOIN mdl_question_coderunner_options o on o.questionid = q.id
+WHERE crs.shortname = 'COSC131-25S1'
+AND o.templateparams like 'precheckers:';
