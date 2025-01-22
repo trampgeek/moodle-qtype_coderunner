4e3a951121052434b4e6b7f4e74260b3a1b2933c
Richard Lobb <trampgeek@gmail.com>
2025-01-22 21:49:35 +1300

    Uupdate Twig to latest version.

--- vendor/twig/twig/src/Extension/DebugExtension.php
+++ vendor/twig/twig/src/Extension/DebugExtension.php
@@ -9,36 +9,34 @@
  * file that was distributed with this source code.
  */
 
-namespace Twig\Extension {
-    use Twig\TwigFunction;
+namespace Twig\Extension;
 
-    final class DebugExtension extends AbstractExtension
+use Twig\Environment;
+use Twig\Template;
+use Twig\TemplateWrapper;
+use Twig\TwigFunction;
+
+final class DebugExtension extends AbstractExtension
+{
+    public function getFunctions(): array
     {
-        public function getFunctions(): array
-        {
-            // dump is safe if var_dump is overridden by xdebug
-            $isDumpOutputHtmlSafe = \extension_loaded('xdebug')
-            // false means that it was not set (and the default is on) or it explicitly enabled
-            && (false === ini_get('xdebug.overload_var_dump') || ini_get('xdebug.overload_var_dump'))
-            // false means that it was not set (and the default is on) or it explicitly enabled
-            // xdebug.overload_var_dump produces HTML only when html_errors is also enabled
-            && (false === ini_get('html_errors') || ini_get('html_errors'))
+        // dump is safe if var_dump is overridden by xdebug
+        $isDumpOutputHtmlSafe = \extension_loaded('xdebug')
+            // Xdebug overloads var_dump in develop mode when html_errors is enabled
+            && str_contains(\ini_get('xdebug.mode'), 'develop')
+            && (false === \ini_get('html_errors') || \ini_get('html_errors'))
             || 'cli' === \PHP_SAPI
-            ;
+        ;
 
-            return [
-            new TwigFunction('dump', 'twig_var_dump', ['is_safe' => $isDumpOutputHtmlSafe ? ['html'] : [], 'needs_context' => true, 'needs_environment' => true, 'is_variadic' => true]),
-            ];
-        }
+        return [
+            new TwigFunction('dump', [self::class, 'dump'], ['is_safe' => $isDumpOutputHtmlSafe ? ['html'] : [], 'needs_context' => true, 'needs_environment' => true, 'is_variadic' => true]),
+        ];
     }
-}
 
-namespace {
-    use Twig\Environment;
-    use Twig\Template;
-    use Twig\TemplateWrapper;
-
-    function twig_var_dump(Environment $env, $context, ...$vars)
+    /**
+     * @internal
+     */
+    public static function dump(Environment $env, $context, ...$vars)
     {
         if (!$env->isDebug()) {
             return;
