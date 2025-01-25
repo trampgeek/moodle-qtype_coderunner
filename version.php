c705051b54dbd6291d1bee9f624ad47608a57d19
Richard Lobb <trampgeek@gmail.com>
2025-01-25 17:11:09 +1300

    Fix bad comment

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2025012300;
-$plugin->requires = 2023100900; # Moodle 4.3
+$plugin->requires = 2023100900; // Moodle 4.3
 $plugin->cron = 0;
 $plugin->component = 'qtype_coderunner';
 $plugin->maturity = MATURITY_STABLE;
